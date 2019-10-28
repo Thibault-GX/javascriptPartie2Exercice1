c94b5d67b0b98b00bd3b5c2ff3e1cd37c5c7e470
Thibault-GX <thibault.grimoux@gmail.com>
2019-10-28 10:01:11 +0100

    Push initial.

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//Définit l'image lorsque la souris survole l'élément
+function passageDeLaSouris(element) {
+element.setAttribute('src', 'img/image1_2.jpg');
+}
+//Définit l'image lorsque la souris ne survole pas l'élément
+function departDeLaSouris(element) {
+element.setAttribute('src', 'img/image1.jpg');
+}
