e6a5e34f21fd2db5ac5054adca586fc75c944a8c
Thibault-GX <55696233+Thibault-GX@users.noreply.github.com>
2019-10-29 14:32:20 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-//Définit l'image lorsque la souris survole l'élément
-function pictureChange(element) {
-element.setAttribute('src', 'img/image1_2.jpg');
-}
-//Définit l'image lorsque la souris ne survole pas l'élément
-function resetPictureChange(element) {
-element.setAttribute('src', 'img/image1.jpg');
-}
